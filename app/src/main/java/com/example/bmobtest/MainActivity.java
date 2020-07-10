f47b57c63475fde03a7bb89ce39bb3b2bd1218ab
UJ <839753488@qq.com>
2020-07-10 09:37:56 +0800

    Bmob initialize.

--- app/src/main/java/com/example/bmobtest/MainActivity.java
+++ app/src/main/java/com/example/bmobtest/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cn.bmob.v3.Bmob;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bmob.initialize(this, "1bfc00caac31c7809a1a6f25d8c1396f");
     }
 }
